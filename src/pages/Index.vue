a62c3384c5bf6d91099f7163580a2fba95330c76
Lucien Bénié <lucien.benie@logmein.com>
2020-03-22 13:23:49 -0400

    update to senior dev

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,13 +6,14 @@
           <h1 class="text-6xl font-bold">Hello! 👋</h1>
           <p class="text-3xl">My name is <strong>Lucien Bénié</strong>.</p>
           <p class="pt-5 leading-loose">
-            I'm a Front-End Developer based in Québec City, Québec, Canada. I've
-            started doing web development back in 2014. I've had the opportunity
-            to work with modern frameworks like <strong>angular.js</strong>,
-            <strong>angular</strong>, <strong>react</strong> &amp;
-            <strong>vue.js</strong>. Also, I'm a proud father of a 2-year-old. I
-            like watching movies, sports and listening to music. In my spare
-            time, I like having a great time with my family and friends.
+            I'm a Senior Front-End Developer based in Québec City, Québec,
+            Canada. I've started doing web development back in 2014. I've had
+            the opportunity to work with modern frameworks like
+            <strong>angular.js</strong>, <strong>angular</strong>,
+            <strong>react</strong> &amp; <strong>vue.js</strong>. Also, I'm a
+            proud father of a 2-year-old. I like watching movies, sports and
+            listening to music. In my spare time, I like having a great time
+            with my family and friends.
           </p>
         </div>
         <g-image
