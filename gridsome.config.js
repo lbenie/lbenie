350b7421d5ec6204f0febccfebb5473945d1ed26
Lucien Bénié <lucien.benie@logmein.com>
2020-11-20 18:04:46 -0500

    tailwind 2.0

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,9 +4,24 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const tailwind = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+const postcssPlugins = [tailwind()]
+
+if (process.env.NODE_ENV === 'production')
+  postcssPlugins.push(purgecss(require('./purgecss.config.js')))
+
 module.exports = {
   titleTemplate: '%s',
   siteUrl: 'https://lbenie.xyz',
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: postcssPlugins,
+      },
+    },
+  },
   plugins: [
     {
       use: '@gridsome/plugin-google-analytics',
@@ -46,9 +61,9 @@ module.exports = {
         typeName: 'Contentful',
       },
     },
-    {
-      use: 'gridsome-plugin-tailwindcss',
-    },
+    // {
+    //   use: 'gridsome-plugin-tailwindcss',
+    // },
     {
       use: 'gridsome-plugin-feed',
       options: {
