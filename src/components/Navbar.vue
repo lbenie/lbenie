3079a1f37af29cd9bb18598574ae975056a8fe6f
Lucien Bénié <lbenie@Luciens-MacBook-Pro-2.local>
2022-03-12 10:47:52 -0500

    sidebar finished

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import type { Component } from 'vue'
 import { useContentful } from '~/hooks'
 import type { NavigationCollection } from '~/models'
+import IconCarbonHome from '~icons/carbon/home?width=2em&height=2em'
+import IconGradutation from '~icons/ph/graduation-cap-light?width=2em&height=2em'
+import IconGlobe from '~icons/fluent/globe-person-20-regular?width=2em&height=2em'
+import IconSuitcase from '~icons/la/suitcase?width=2em&height=2em'
+import IconBook from '~icons/clarity/book-line?width=2em&height=2em'
 
 const query = `
   {
@@ -14,6 +20,14 @@ const query = `
 `
 
 const { data } = useContentful<NavigationCollection>(query)
+
+const iconMap: Record<string, Component> = {
+  home: IconCarbonHome,
+  resume: IconGradutation,
+  'open-source': IconGlobe,
+  projects: IconSuitcase,
+  blog: IconBook,
+}
 </script>
 
 <template>
@@ -22,11 +36,12 @@ const { data } = useContentful<NavigationCollection>(query)
       <li
         v-for="({ name, slug }, index) in data?.items"
         :key="index"
-        class="my-auto mr-1 ml-1 text-inherit"
+        class="my-2 mr-1 ml-1 text-inherit flex flex-col items-center"
       >
         <router-link :to="`/${slug ?? ''}`" class="p-1 no-underline capitalize">
-          {{ name }}
+          <component :is="iconMap[name]" />
         </router-link>
+        <p class="text-xs">{{ name }}</p>
       </li>
       <!-- <li
         v-if="resume"
