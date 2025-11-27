98c77467c642861871d802d9f3910739a8e5dc8b
Lucien Benie <lucien.benie@goto.com>
2025-11-27 17:43:54 -0500

    fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,6 @@ interface ImportMetaEnv {
   readonly CONTENTFUL_SPACE_ID: string
   readonly CONTENTFUL_DELIVERY_TOKEN: string
   readonly CONTENTFUL_PREVIEW_TOKEN: string
-  readonly CONTENTFUL_MANAGEMENT_TOKEN: string
   readonly DEV: boolean
 }
 
